43638ffa0a5163db09d183dd2fced3fbb7e08933
Vamsikrishna <141436747+Vamsi0935@users.noreply.github.com>
2024-08-29 16:34:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://video-streaming-app-frontend-blond.vercel.app", credentials: true }));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
